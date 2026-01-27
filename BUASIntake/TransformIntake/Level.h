6a775ba0c0a0e9be5bb729066edaa246ac8bae69
Kevin Assink <xshadowfighterx943@gmail.com>
2026-01-27 22:35:45 +0100

    Created the pathnode class and setup the base level logic

--- BUASIntake/TransformIntake/Level.h
+++ BUASIntake/TransformIntake/Level.h
@@ -1,5 +1,38 @@
 #pragma once
+#include <vector>
+#include <memory>
+#include "Enemy.h"
+#include "Tower.h"
+#include "PathNode.h"
+#include "WaveLogic.h"
+
 class Level
 {
+public:
+	Level(const LevelConfig& config);
+	void update(float dt);
+	void render();
+
+	bool isCompleted() const { return completed; }
+	bool isFailed() const { return failed; }
+
+	static Level createCitySmogLevel();
+
+private:
+	void SpawnEnemies(float dt);
+	void UpdateEnemies(float dt);
+	void UpdateTowers(float dt);
+	void handleCollision();
+
+
+	LevelConfig config;
+	std::vector<std::unique_ptr<Enemy>> enemies;
+	std::vector<std::unique_ptr<Tower>> towers;
+
+	float waveTimer = 0.0f;
+	int currentWaveIndex = 0;
+	int EcoSystemHealth;
+	bool completed = false;
+	bool failed = false;
 };
 
