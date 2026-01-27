3bc78644c68ae48b0542c8b773a3ce2f84763fa1
Kevin Assink <xshadowfighterx943@gmail.com>
2026-01-27 22:23:24 +0100

    Created the core workings of the game excluding the input

--- BUASIntake/TransformIntake/Game.cpp
+++ BUASIntake/TransformIntake/Game.cpp
@@ -1,1 +1,47 @@
 #include "Game.h"
+
+Game::Game() {}
+
+bool Game::init() {
+	// Here the levels, assets etc will be loaded into the game
+
+	levels.emplace_back(Level::createCitySmogLevel());
+	running = true;
+	return true;
+}
+
+
+void Game::run() {
+
+//This will handle the running of the game
+	const float fixedDt = 1.0f / 60.0f;
+	while (running) {
+		handleInput();
+		update(fixedDt);
+		render();
+	}
+}
+
+void Game::handleInput() {
+	//This will handle the input of the game
+}
+
+void Game::update(float dt){
+	if (currentLevelIndex < 0 || currentLevelIndex >= (int)Levels.size()) return;
+	Level& lvl = levels[currentLevelIndex];
+
+	// this handles the level quiting if there are no more levels or if the game is failed
+	lvl.update(dt);
+	if (lvl.isCompleted()) {
+		++currentLevelIndex;
+		if (currentLevelIndex >= (int)Levels.size()) {
+			quit();
+		}
+	}
+}
+
+void Game::render() {
+	//clear screen
+	//levels[currentlevelindex]
+	//present
+}
