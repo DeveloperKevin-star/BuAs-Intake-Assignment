de310aba2ef23804120aafbfd03b1983fd1b80c6
Kevin Assink <xshadowfighterx943@gmail.com>
2026-01-27 21:59:36 +0100

    Fixed a small mistake where the game.h was made in the game.cpp

--- BUASIntake/TransformIntake/Game.h
+++ BUASIntake/TransformIntake/Game.h
@@ -1,5 +1,22 @@
 #pragma once
+#include <memory>
+#include <vector>
+#include "Level.h"
 class Game
 {
+public:
+	Game();
+	bool init();
+	void run();
+	void quit() { running = false; }
+
+private:
+	void handleInput();
+	void update(float dt);
+	void render();
+
+	bool running = false;
+	std::vector<Level> levels;
+	int currentLevelIndex = 0;
 };
 
