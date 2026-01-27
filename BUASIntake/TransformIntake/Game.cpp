bcfcac068897573747619baaa3fa4b89520243a1
Kevin Assink <xshadowfighterx943@gmail.com>
2026-01-27 21:54:23 +0100

    Added the base Game classes and added the clas logic of the rest of the game

---
+++ BUASIntake/TransformIntake/Game.cpp
@@ -0,0 +1,22 @@
+#include "Game.h"
+#pragma once
+#include <memory>
+#include <vector>
+#include "Level.h"
+
+class Game {
+public:
+	Game();
+	bool init();
+	void run();
+	void quit() { running = false;  }
+
+private:
+	void handleInput();
+	void update(float dt);
+	void render();
+
+	bool running = false;
+	std::vector<Level> levels;
+	int currentLevelIndex = 0;
+};
