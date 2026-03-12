e0aba7aa621ec81e85630c082a3990fa9379d0b2
Kevin Assink <xshadowfighterx943@gmail.com>
2026-03-12 18:01:44 +0100

    created the base for the tower fire flash

--- BUASIntake/TransformIntake/Tower.h
+++ BUASIntake/TransformIntake/Tower.h
@@ -16,6 +16,8 @@ public:
     float getX() const { return x; }
     float getY() const { return y; }
     float getRange() const { return range; }
+    float getFireFlashTimer() const { return fireFlashTimer; }
+    float getFireFlashDuration() const { return fireFlashDuratio; }
 
 private:
     float x = 0.0f;
@@ -24,6 +26,8 @@ private:
     float fireRate = 1.0f;
     float fireCooldown = 0.0f;
     int damage = 1;
+    float fireFlashTimer = 0.0f;
+    float fireFlashDuratio = 0.08f;
 
     Enemy* findTarget(std::vector<std::unique_ptr<Enemy>>& enemies);
 };
